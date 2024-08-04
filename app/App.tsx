972bd648e4861ca3b6602507fc9c0c004a6679ff
Thalisson <thalissonsousa33@gmail.com>
2024-08-03 23:45:08 -0300

    fix: remove header

--- app/App.tsx
+++ app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';   
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './components/home/Home';
 import Questionario from './components/questionario/Questionario';
 import RankingScreen from './components/ranking/Ranking';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -14,19 +16,21 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen   
- name="Home" component={Home}   
+        <Stack.Screen
+ name="Home" component={Home}
  />
-        <Stack.Screen name="Questionario" component={QuestionarioStack} />
+        <Stack.Screen name="Questionario" component={AppNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const QuestionarioStack = () => {
+const AppNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Questionário" component={Questionario} />
+    <Tab.Navigator
+      screenOptions={{headerShown: false }}
+     >
+      <Tab.Screen name="Questionario" component={Questionario} />
       <Tab.Screen name="Ranking" component={RankingScreen} />
     </Tab.Navigator>
   );
